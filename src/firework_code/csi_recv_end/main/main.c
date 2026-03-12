3b93b12220f0768ed517a524eeedb6e3578ba8e2
Lixeer <1612655510@qq.com>
2026-03-13 02:44:38 +0800

    first commit

---
+++ src/firework_code/csi_recv_end/main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+void app_main(void)
+{
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    printf("hello ChanSense\n");
+
+    
+}
